e7ab1551d66e613a74c55fd5a8e6cb315d4964a0
renkdomingues <renkdomingues@gmail.com>
2023-03-29 21:09:49 -0300

    Novas modificações com formulários

---
+++ UpdateSeleniumWebDriver/FrmDownload.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace UpdateSeleniumWebDriver
+{
+    partial class frmDownload
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.prgBarDownload = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlDownload = new System.Windows.Forms.Panel();
+            this.pnlArqExiste = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlDownload.SuspendLayout();
+            this.pnlArqExiste.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // prgBarDownload
+            // 
+            this.prgBarDownload.Location = new System.Drawing.Point(7, 143);
+            this.prgBarDownload.Name = "prgBarDownload";
+            this.prgBarDownload.Size = new System.Drawing.Size(418, 23);
+            this.prgBarDownload.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prgBarDownload.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(106, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Download em progresso...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(137, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seu download finalizará logo!";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Enabled = false;
+            this.btnFechar.Location = new System.Drawing.Point(185, 262);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 3;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblStatus.Location = new System.Drawing.Point(193, 11);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(51, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Aguarde";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStatus.TextChanged += new System.EventHandler(this.lblStatus_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlDownload
+            // 
+            this.pnlDownload.Controls.Add(this.label1);
+            this.pnlDownload.Controls.Add(this.prgBarDownload);
+            this.pnlDownload.Controls.Add(this.label2);
+            this.pnlDownload.Location = new System.Drawing.Point(4, 11);
+            this.pnlDownload.Name = "pnlDownload";
+            this.pnlDownload.Size = new System.Drawing.Size(434, 186);
+            this.pnlDownload.TabIndex = 5;
+            this.pnlDownload.Visible = false;
+            // 
+            // pnlArqExiste
+            // 
+            this.pnlArqExiste.Controls.Add(this.label3);
+            this.pnlArqExiste.Location = new System.Drawing.Point(4, 11);
+            this.pnlArqExiste.Name = "pnlArqExiste";
+            this.pnlArqExiste.Size = new System.Drawing.Size(434, 186);
+            this.pnlArqExiste.TabIndex = 6;
+            this.pnlArqExiste.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(42, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(331, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "A versão mais recente já está baixada!";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Location = new System.Drawing.Point(4, 219);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 37);
+            this.panel1.TabIndex = 7;
+            // 
+            // frmDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 297);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.pnlDownload);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlArqExiste);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmDownload";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebDriver Downloader";
+            this.pnlDownload.ResumeLayout(false);
+            this.pnlDownload.PerformLayout();
+            this.pnlArqExiste.ResumeLayout(false);
+            this.pnlArqExiste.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ProgressBar prgBarDownload;
+        private Label label1;
+        private Label label2;
+        private Button btnFechar;
+        private Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+        private Panel pnlDownload;
+        private Panel pnlArqExiste;
+        private Label label3;
+        private Panel panel1;
+    }
+}
--- UpdateSeleniumWebDriver/FrmDownload.cs
+++ UpdateSeleniumWebDriver/FrmDownload.cs
@@ -1,20 +1,24 @@
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace UpdateSeleniumWebDriver
 {
-    public partial class Form1 : Form
+    public partial class frmDownload : Form
     {
         static string version;
-        public Form1()
+        static string driver = "";
+
+        public frmDownload()
         {
             InitializeComponent();
-            teste();
+            Download();
         }
 
-        private void teste()
+        private void Download()
         {
+
             var psi = new ProcessStartInfo("wmic");
             psi.Arguments = @"datafile where 'name=""C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe""' get version";
             psi.WindowStyle = ProcessWindowStyle.Hidden;
@@ -32,7 +36,7 @@ namespace UpdateSeleniumWebDriver
                 DownloadWebDriver();
             }
         }
-        static string driver = "";
+        
         private void DownloadWebDriver()
         {
             var downloadURL = "https://msedgedriver.azureedge.net/" + version + "/edgedriver_win32.zip";
@@ -54,20 +58,57 @@ namespace UpdateSeleniumWebDriver
                         File.Delete(item);
                 }
 
+                pnlDownload.Visible = true;
                 DownloadFileInBackGround(downloadURL);
             }
-            
+            else
+            {
+                pnlArqExiste.Visible = true;
+                btnFechar.Enabled = true;
+                lblStatus.Text = "Para continuar, feche este formulário.";
+            }
+               
 
         }
 
-        private static void DownloadFileInBackGround(string url)
+        private void DownloadFileInBackGround(string url)
         {
+            timer1.Start();
             using (var cliente = new WebClient())
             {
                 cliente.DownloadFile(url, Path.Combine(Directory.GetCurrentDirectory(), "edgedriver_" + version + ".zip"));
-
                 ZipFile.ExtractToDirectory(driver, Directory.GetCurrentDirectory());
+
+                if(File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "msedgedriver.exe")))
+                {
+                    btnFechar.Enabled = true;
+                    lblStatus.Text = "Download feito com sucesso, feche este formulário.";
+                    prgBarDownload.Style = ProgressBarStyle.Continuous;
+                    prgBarDownload.MarqueeAnimationSpeed = 0;
+                }
             }
+            timer1.Stop();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(prgBarDownload.Value >= 100)
+                prgBarDownload.Value = 100;
+            else
+                prgBarDownload.Value += 5;
+        }
+
+        private void lblStatus_TextChanged(object sender, EventArgs e)
+        {
+            int x = (panel1.Size.Width - lblStatus.Width) / 2;
+            int y = (panel1.Size.Height - lblStatus.Height) / 2;
+
+            lblStatus.Location = new Point(x, y);
         }
     }
 }
